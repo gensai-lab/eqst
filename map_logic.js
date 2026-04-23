eaa55d8d64ba81a42815efb31679756c76eead98
みんなの減災ラボ <jridpm@gmail.com>
2026-04-23 10:10:55 +0900

    Update map_logic.js

--- map_logic.js
+++ map_logic.js
@@ -1,8 +1,8 @@
 // map_logic.js
 
 // --- 【設定】ここでアイコンの大きさを調整できます ---
-const ICON_SIZE = 15;      // 震度アイコンのサイズ（px）
-const SHINGEN_SIZE = 20;   // 震源アイコンのサイズ（px）
+const ICON_SIZE = 8;      // 震度アイコンのサイズ（px）
+const SHINGEN_SIZE = 6;   // 震源アイコンのサイズ（px）
 // ------------------------------------------------
 
 let pointsData = {};
